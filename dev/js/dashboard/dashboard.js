15a963ac7700538d9bdfad3d0edb57a633fe51ae
SayanaHere <63317960+SayanaHere@users.noreply.github.com>
2020-11-22 18:52:44 -0500

    Dashboard added

--- dev/js/dashboard/dashboard.js
+++ dev/js/dashboard/dashboard.js
@@ -8,15 +8,86 @@ gsap.registerPlugin(DrawSVGPlugin);
 const dashboardtl = gsap.timeline();
 
 export function DashboardAnimation() {
+
     
     dashboardtl.to("#Logo", {
-        duration: 1,
+        duration: 0.3,
         y: "-=50",
         x: "+=233",
 
         scale:0.2 });
-        
-        
 
+
+    dashboardtl.from("#Speedometer", {
+            duration: 0.9,
+           alpha:-1
+           
+         });
+
+    dashboardtl.from("#RPM", {
+            duration: 1,
+           alpha:-1
+         });
+
+    dashboardtl.from("#Speed", {
+            duration: 0.9,
+           alpha:-1
+         });
+
+    dashboardtl.from("#Circles", {
+            duration: 1,
+           alpha:-1
+         });
+
+    dashboardtl.from("#Icon", {
+            duration: 1,
+           alpha:-1
+         });
+    
+         dashboardtl.from("#Music", {
+            duration: 0.6,
+           alpha:-1
+         });
+         dashboardtl.from("#Settings", {
+            duration: 1,
+           alpha:-1
+         });
+         dashboardtl.from("#Brightness", {
+            duration: 1,
+           alpha:-1
+         });
+         dashboardtl.from("#AC", {
+            duration: 1,
+           alpha:-1
+         });
+         dashboardtl.from("#Fan", {
+            duration: 1,
+           alpha:-1
+         });
+         dashboardtl.from("#Temperaturefuel-", {
+            duration: 1,
+           alpha:-1
+         });
+         dashboardtl.from("#Emergency", {
+            duration: 0.7,
+           alpha:-1
+         });
+         dashboardtl.from("#Fuel", {
+            duration: 0.7,
+           alpha:-1
+         });
+         dashboardtl.from("#Temperature", {
+            duration: 0.7,
+           alpha:-1
+         });
+        
+         dashboardtl.from("#Speed-Needle", {
+            duration: 4,
+           alpha:-1,
+           rotate: 800
+           
+         });
     return dashboardtl;
+
+    
 }
--- dev/js/logo/logo.js
+++ dev/js/logo/logo.js
@@ -16,36 +16,36 @@ const logotl = gsap.timeline();
 export function LogoAnimation() {
 
     logotl.from("#circle-1", {
-        duration: 1,
+        duration: 0.8,
         drawSVG: "50% 50%"
     });
 
     logotl.from("#circle-2", {
-        duration: 1.3,
+        duration: 0.8,
         drawSVG: "50% 50%"
     });
 
     logotl.from("#circle-3", {
-        duration: 1.6,
+        duration: 0.8,
         drawSVG: "50% 50%"
     });
 
     logotl.from("#circle-4", {
-        duration: 2,
+        duration: 0.8,
         drawSVG: "50% 50%"
     });
 
     logotl.from("#Audi", {
-
-        duration: 1.6,
+        duration: 0.8,
         drawSVG: "50% 50%"
-        // y: -20
     });
 
     logotl.to("#Audi-Logo", {
-        duration: 1,
+        duration: 0.8,
         y: 90
     });
+
+    
 
     return logotl;
 }
